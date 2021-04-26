2ed48934579d81ce0872140802c911156ff4bc94
serophiom <serophiom@gmail.com>
2021-04-26 20:08:02 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,7 +3,26 @@ import gallery from './gallery-items.js';
 const galleryUlRef = document.querySelector('.js-gallery');
 console.log(galleryUlRef);
 
-gallery.map(image => {
-  galleryUlRef.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class= "gallery__link" href ="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"></a></li>`);
+// gallery.map(image => {
+//   galleryUlRef.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class= "gallery__link" href ="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"></a></li>`);
   
-})
+// })
+
+function createGallery = gallery.map(image => {
+const galleryRef = `<li class="gallery__item">
+<a class="gallery__link"
+href="${image.original}">
+<img class="gallery__image"
+src="${image.preview}"
+data-source = "${image.original}"
+alt="image.description">
+</a>
+</li>`;
+console.log(galleryRef);
+return galleryRef;
+});
+
+const createGalleryRefs = 
+
+
+
