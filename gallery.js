d0e81c31d3883f508dcfd2661836e61c6a5d4ad5
serophiom <serophiom@gmail.com>
2021-04-26 23:43:04 +0300

    update 8 DZ

--- gallery.js
+++ gallery.js
@@ -1,28 +1,31 @@
 import gallery from './gallery-items.js';
-
+console.log(gallery);
 const galleryUlRef = document.querySelector('.js-gallery');
 console.log(galleryUlRef);
 
-// gallery.map(image => {
-//   galleryUlRef.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class= "gallery__link" href ="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"></a></li>`);
-  
-// })
+const createGalleryRefs = createGallery(gallery);
 
-function createGallery = gallery.map(image => {
-const galleryRef = `<li class="gallery__item">
-<a class="gallery__link"
-href="${image.original}">
+galleryUlRef.insertAdjacentHTML('beforeend', createGalleryRefs);
+
+
+
+function createGallery(gallery) {
+return gallery.map(({original, preview, description} = {}) => {
+return `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
 <img class="gallery__image"
-src="${image.preview}"
-data-source = "${image.original}"
+src="${preview}"
+data-source="${original}"
 alt="image.description">
 </a>
-</li>`;
-console.log(galleryRef);
-return galleryRef;
-});
+</li>`
+})
+.join('');
+};
 
-const createGalleryRefs = 
+
+
+
 
 
 
