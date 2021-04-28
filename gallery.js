002ef37bc5d025a2e30f5b706d82a7cdfd297c40
serophiom <serophiom@gmail.com>
2021-04-28 07:59:59 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,12 +2,37 @@ import gallery from './gallery-items.js';
 console.log(gallery);
 const galleryUlRef = document.querySelector('.js-gallery');
 console.log(galleryUlRef);
+const modalRef = document.querySelector('.js-lightbox');
+console.log(modalRef);
+const btnCloseModal = document.querySelector('[data-action="close-lightbox"]');
+console.log(btnCloseModal);
 
 const createGalleryRefs = createGallery(gallery);
 
 galleryUlRef.insertAdjacentHTML('beforeend', createGalleryRefs);
 
+galleryUlRef.addEventListener('click', onOpenModal);
 
+btnCloseModal.addEventListener('click', onCloseModal);
+
+// const getOriginalImage = galleryUlRef.addEventListener('click', onTargetCkick);
+
+// function onTargetCkick () {
+//     const originalImage = gallery.querySelector('data-resource');
+//     console.log(originalImage);
+// }
+
+function onOpenModal (event) {
+    const isGalleryItemRef = event.target.classList.contains('gallery__item');
+    if (!isGalleryItemRef) {
+        return;
+    }
+  modalRef.classList.add('is-open');
+}
+
+function onCloseModal () {
+    modalRef.classList.remove('is-open');
+}
 
 function createGallery(gallery) {
 return gallery.map(({original, preview, description} = {}) => {
