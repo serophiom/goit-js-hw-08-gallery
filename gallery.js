20d03191a40e5ba91eae93815f3388d65899af26
serophiom <serophiom@gmail.com>
2021-04-30 23:30:42 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -17,16 +17,18 @@ galleryUlRef.insertAdjacentHTML('beforeend', createGalleryRefs);
 galleryUlRef.addEventListener('click', onOpenModal);
 
 btnCloseModal.addEventListener('click', onCloseModal);
-overlayRef.addEventListener('click', onCloseModal);
-window.addEventListener('keydown', onCloseModal);
+overlayRef.addEventListener('click', onOverlayClick);
+window.addEventListener('keydown', onEscapeKeyPress);
 
 function closeModalEscape (event) {
     
 }
 
 function onOpenModal (event) {
+    window.addEventListener('keydown', onEscapeKeyPress);
+    event.preventDefault();
     const isGalleryItemRef = event.target.classList.contains('gallery__image');
-    if (!isGalleryItemRef || event.currentTarget === event.target) {
+    if (!isGalleryItemRef) {
         console.log('AGA');
         return;
     }
@@ -37,20 +39,53 @@ function onOpenModal (event) {
 }
 
 function onCloseModal () {
+    window.removeEventListener('keydown', onEscapeKeyPress);
     modalRef.classList.remove('is-open');
     modalImageRef.src = '';
     console.log(modalImageRef.src);
 }
 
-function createGallery(gallery) {
-return gallery.map(({original, preview, description} = {}) => {
-return `<li class="gallery__item">
+function onOverlayClick (event) {
+    if (event.currentTarget === event.target) {
+        onCloseModal ();
+    }
+}
 
+function onEscapeKeyPress (event) {
+    console.log(event.code);
+    // ArrowRight
+    // ArrowLeft
+    if (event.code === 'Escape') {
+        onCloseModal ();
+    }
+
+    if (event.code === 'ArrowRight') {
+        console.log(gallery[1]);
+        const i = 1
+        // console.log('Right');
+        modalImageRef.src = gallery[i+1].original;
+        // console.log(modalImageRef.src);
+    }
+
+    if (event.code === 'ArrowLeft') {
+        console.log('Left');
+    }
+}
+
+// function onArrowKeyPress (event) {
+//     console.log(event.code);
+// }
+
+function createGallery(gallery) {
+return gallery.map(({original, preview, description, index} = {}) => {
+return `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
 <img class="gallery__image"
 src="${preview}"
 data-source="${original}"
+data-index="${index}"
 alt="${description}">
-
+</a>
 </li>`
 })
 .join('');
@@ -58,6 +93,7 @@ alt="${description}">
 
 // <a class="gallery__link" href="${original}"></a>
 // </a>
+// || event.currentTarget === event.target
 
 
 
