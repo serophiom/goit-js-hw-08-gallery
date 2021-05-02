91bd9b335f63b73f913c4f2fbb360f48a0a6930c
serophiom <serophiom@gmail.com>
2021-05-02 16:41:39 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,48 +1,39 @@
 import gallery from './gallery-items.js';
-console.log(gallery);
-const galleryUlRef = document.querySelector('.js-gallery');
-console.log(galleryUlRef);
-const modalRef = document.querySelector('.js-lightbox');
-console.log(modalRef);
-const btnCloseModal = document.querySelector('[data-action="close-lightbox"]');
-console.log(btnCloseModal);
-const modalImageRef = document.querySelector('.lightbox__image');
-console.log(modalImageRef);
-const createGalleryRefs = createGallery(gallery);
-const overlayRef = document.querySelector('.lightbox__overlay');
-console.log(overlayRef);
+const imageArray = [];
 
-galleryUlRef.insertAdjacentHTML('beforeend', createGalleryRefs);
-
-galleryUlRef.addEventListener('click', onOpenModal);
-
-btnCloseModal.addEventListener('click', onCloseModal);
-overlayRef.addEventListener('click', onOverlayClick);
-window.addEventListener('keydown', onEscapeKeyPress);
-
-function closeModalEscape (event) {
-    
+const refs = {
+    galleryUlRef: document.querySelector('.js-gallery'),
+    modalRef: document.querySelector('.js-lightbox'),
+    btnCloseModal: document.querySelector('[data-action="close-lightbox"]'),
+    modalImageRef: document.querySelector('.lightbox__image'),
+    createGalleryRefs: createGallery(gallery),
+    overlayRef: document.querySelector('.lightbox__overlay'),
 }
+
+refs.galleryUlRef.insertAdjacentHTML('beforeend', refs.createGalleryRefs);
+
+refs.galleryUlRef.addEventListener('click', onOpenModal);
+
+refs.btnCloseModal.addEventListener('click', onCloseModal);
+
+refs.overlayRef.addEventListener('click', onOverlayClick);
 
 function onOpenModal (event) {
     window.addEventListener('keydown', onEscapeKeyPress);
     event.preventDefault();
     const isGalleryItemRef = event.target.classList.contains('gallery__image');
     if (!isGalleryItemRef) {
-        console.log('AGA');
         return;
     }
-  modalRef.classList.add('is-open');
-  modalImageRef.src = event.target.dataset.source;
-  modalImageRef.alt = event.target.alt;
-  console.log(modalImageRef.src);
+  refs.modalRef.classList.add('is-open');
+  refs.modalImageRef.src = event.target.dataset.source;
+  refs.modalImageRef.alt = event.target.alt;
 }
 
 function onCloseModal () {
     window.removeEventListener('keydown', onEscapeKeyPress);
-    modalRef.classList.remove('is-open');
-    modalImageRef.src = '';
-    console.log(modalImageRef.src);
+    refs.modalRef.classList.remove('is-open');
+    refs.modalImageRef.src = '';
 }
 
 function onOverlayClick (event) {
@@ -52,51 +43,38 @@ function onOverlayClick (event) {
 }
 
 function onEscapeKeyPress (event) {
-    console.log(event.code);
-    // ArrowRight
-    // ArrowLeft
     if (event.code === 'Escape') {
         onCloseModal ();
     }
 
     if (event.code === 'ArrowRight') {
-        console.log(gallery[1]);
-        const i = 1
-        // console.log('Right');
-        modalImageRef.src = gallery[i+1].original;
-        // console.log(modalImageRef.src);
+        if (imageArray.indexOf(refs.modalImageRef.src) === imageArray.length - 1) {
+            return;
+        }
+        refs.modalImageRef.src = imageArray[imageArray.indexOf(refs.modalImageRef.src) + 1];
     }
 
     if (event.code === 'ArrowLeft') {
-        console.log('Left');
+        if (imageArray.indexOf(refs.modalImageRef.src) === 0) {
+            return;
+        }
+        refs.modalImageRef.src = imageArray[imageArray.indexOf(refs.modalImageRef.src) - 1];
     }
 }
 
-// function onArrowKeyPress (event) {
-//     console.log(event.code);
-// }
-
 function createGallery(gallery) {
-return gallery.map(({original, preview, description, index} = {}) => {
-return `<li class="gallery__item">
-<a class="gallery__link" href="${original}">
-<img class="gallery__image"
-src="${preview}"
-data-source="${original}"
-data-index="${index}"
-alt="${description}">
-</a>
-</li>`
+    return gallery.map(({original, preview, description, index} = {}) => {
+    imageArray.push(`${original}`);
+
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    data-index="${index}"
+    alt="${description}">
+    </a>
+    </li>`
 })
 .join('');
-};
-
-// <a class="gallery__link" href="${original}"></a>
-// </a>
-// || event.currentTarget === event.target
-
-
-
-
-
-
+}
